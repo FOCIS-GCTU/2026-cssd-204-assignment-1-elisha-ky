ac837b35065e77cc1e122db2b32bbd37960ca193
elisha <2425403083@live.gctu.edu.gh>
2026-05-30 23:04:53 -1200

    submit assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 1
+#
+# Name: Elisha Kwabena Yeboah
+# STUDENT ID:  2425403083
+# Email: 2425403083@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Elisha kwabena yeboah , this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials
+    print()
+    print("...EKY")
+    print()
 
+
+    print("...   EEEEEEEEEE   ....  K           K    ....   Y    Y       ....")
+    print("...   EEEEEEEEEE   ....  K         K      ....   Y    Y       ....")
+    print("...   EE           ....  K       K        ....   Y   Y        ....")
+    print("...   EE           ....   K     K         ....    Y Y        ....")
+    print("...   EEEEEEEEEE   ....   K   K           ....     Y         ....")
+    print("...   EEEEEEEEEE   ....   K K             ....     Y         ....")
+    print("...   EE           ....   K   K           ....     Y         ....")
+    print("...   EE           ....   K     K         ....     Y         ....")
+    print("...   EEEEEEEEEE   ****   K       K       ****     Y         ****")
+    print("...   EEEEEEEEEE   ****   K         K     ****     Y         ****")
+    print()
 
 
 main()
